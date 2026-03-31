ebf34bd2a8f3c9fb08d1c3e995c0703e3dd0cb0d
Md Wasim Uddin <mdwasimu015@gmail.com>
2026-03-31 13:55:17 +0600

    rate limiting

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,12 +1,20 @@
 import { Controller, Get } from '@nestjs/common';
 import { AppService } from './app.service';
+import { Throttle } from '@nestjs/throttler';
 
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
   @Get()
+  @Throttle({
+    ShortTermThrottler: {
+      limit: 3,
+      ttl: 60000,
+    }
+  })
   getHello(): string {
-    return this.appService.getHello();
+    // return this.appService.getHello();
+    return 'This route is rate limited to 3 requests per minute.';
   }
 }
--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,20 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { seconds, ThrottlerGuard, ThrottlerModule } from '@nestjs/throttler';
 
 @Module({
-  imports: [],
+  imports: [
+    ThrottlerModule.forRoot({
+      throttlers: [{
+        name: 'ShortTermThrottler',
+        ttl: seconds(60),
+        limit: 3,
+      }],
+      errorMessage: 'Too many requests, please try again later.',
+    })
+  ],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [AppService, { provide: 'APP_GUARD', useClass: ThrottlerGuard }],
 })
 export class AppModule {}
